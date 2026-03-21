d5ae0dc87ba748ecfad8734fe6552951d97f8dbd
doorcs <itsdoorcs@gmail.com>
2026-03-21 22:11:30 +0900

    회원 서비스 개발

---
+++ lectures/spring-for-beginners/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,36 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        // `Ctrl + T` 단축키로 리팩토링 옵션 열기 -> `Extract Method`
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()) // 같은 이름을 가지는 중복 회원 확인
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 서비스 클래스의 메서드명은 `비즈니스 로직`에 가깝게 짓는게 좋다
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
