d1a72d05a67929e8f09b339d5c59f3ebbf546e3e
doorcs <itsdoorcs@gmail.com>
2026-03-22 21:21:35 +0900

    JPA
    JPA는 모든 데이터 변경이 트랜잭션 안에서 실행돼야 한다!
    
    그래서 서비스 계층에 `@Transcational`을 꼭 붙여줘야함
    (엄밀하게는 DB 데이터를 수정하는 메서드)

--- lectures/spring-for-beginners/src/main/java/hello/hellospring/SpringConfig.java
+++ lectures/spring-for-beginners/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,19 +1,25 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
-import javax.sql.DataSource;
+import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration // 스프링이 뜰 때, 이렇게 `@Configuration` 어노테이션이 붙은 클래스에
 public class SpringConfig {
 
-    private final DataSource dataSource;
+//    private final DataSource dataSource;
+//
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    private final EntityManager entityManager;
+
+    public SpringConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
     @Bean // 이 `@Bean` 어노테이션이 있으면
@@ -25,6 +31,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
         // return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        // return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(entityManager);
     }
 }
