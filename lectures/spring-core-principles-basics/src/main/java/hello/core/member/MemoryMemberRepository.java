cb51d13638b3c1c60d27b3640da7e662685d2ef1
doorcs <itsdoorcs@gmail.com>
2026-03-23 12:17:39 +0900

    회원 도메인 개발

---
+++ lectures/spring-core-principles-basics/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>(); // 실무에서는 ConcurrentHashMap을 써야 함
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member); // 원래는 예외 처리가 들어가야 하는데, 단순한 예제니까 생략
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
