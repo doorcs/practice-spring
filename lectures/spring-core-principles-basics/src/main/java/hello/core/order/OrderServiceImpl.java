2cf555b5b3b40324fcbdf2e3ad45d999b127e894
doorcs <itsdoorcs@gmail.com>
2026-03-25 00:09:11 +0900

    `@Autowired` 필드명, `@Qualifier`, `@Primary`
    타입으로 빈을 조회할 때 두 개 이상의 빈이 조회될 경우 해결방법

--- lectures/spring-core-principles-basics/src/main/java/hello/core/order/OrderServiceImpl.java
+++ lectures/spring-core-principles-basics/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,16 +3,24 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     // 인터페이스에만 의존. 구체적인 클래스에 대해서는 전혀 모름. DIP를 잘 지키고 있는 상태!
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(
+            MemberRepository memberRepository,
+            // `@Primary`랑 `@Qualifier`중에선 `@Qualifier`가 우선순위를 가진다
+            // 따라서 여기서는 `@Primary`인 `rateDiscountPolicy` 빈 대신 `fixDiscountPolicy` 빈이 주입된다
+            @Qualifier("fixDiscountPolicy") DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
