a7dd27440973ee2cd82df86fa18466ee7ffe0cac
doorcs <itsdoorcs@gmail.com>
2026-03-24 23:31:19 +0900

    롬복과 최신 트렌드
    `@RequiredArgsConstructor`
    + `private final` 의존관계
    
    -> 생성자 주입 방식을 사용하면서도 간결한 코드가 나온다

--- lectures/spring-core-principles-basics/src/main/java/hello/core/order/OrderServiceImpl.java
+++ lectures/spring-core-principles-basics/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,25 +3,16 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     // 인터페이스에만 의존. 구체적인 클래스에 대해서는 전혀 모름. DIP를 잘 지키고 있는 상태!
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    // 생성자가 딱 하나만 있으면 `@Autowired` 어노테이션을 생략해도 의존관계가 자동으로 주입된다!
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("1. OrderServiceImpl.OrderServiceImpl");
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-        // 이 생성자를 통해서 어떤 구현 객체가 들어올지(주입될지)는 알 수 없다
-        // OrderServiceImpl은 이제부터 실행에만 집중하면 된다
-
-        // 배우는 자신의 배역을 수행하는 데 집중해야 한다. 다른 배역에 누가 캐스팅될지 신경쓰는 건 배우의 관심사가 아니다!!
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -31,7 +22,6 @@ public class OrderServiceImpl implements OrderService {
         return new Order(member.getId(), itemName, itemPrice, discountPrice);
     }
 
-    // 테스트용
     public MemberRepository getMemberRepository() {
         return memberRepository;
     }
