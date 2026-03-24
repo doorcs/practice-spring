2603316521d8d36abfc56af657336c51b50b4967
doorcs <itsdoorcs@gmail.com>
2026-03-24 18:08:36 +0900

    탐색 위치와 기본 스캔 대상
    `@ComponentScan` 붙는 설정 클래스를 프로젝트 최상단에 둬라

--- lectures/spring-core-principles-basics/src/main/java/hello/core/AutoAppConfig.java
+++ lectures/spring-core-principles-basics/src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+@ComponentScan(
+        // basePackages = "hello.core",
+        // basePackages = {"hello.core.member", "hello.core.order"},
+        // basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 // 실무에서 `@ComponentScan`을 쓰는 경우 excludeFilters 옵션을 안 주는데, 예제에서 만들었던 Config 클래스들을 살려두기 위한 것
 public class AutoAppConfig {}
 
@@ -19,3 +23,9 @@ public class AutoAppConfig {}
 
 // + `@Component` 클래스의 생성자에 `@Autowired` 어노테이션이 있으면, 필요한 의존관계들을 스프링이 자동으로 주입해준다
 // 이때 기본 전략은 `타입이 같은 빈`을 찾아서 주입해주는 것!
+
+// `@ComponentScan`의 탐색 범위를 직접 지정해줄수도 있지만,
+// 생략할 경우 `@ComponentScan` 어노테이션이 붙은 클래스의 위치가 탐색 시작 위치가 된다
+
+// 스캔 위치를 지정해주는 대신, `@ComponentScan`이 붙는 설정 클래스를 프로젝트 최상단에 두는 방식을 권장!
+// 스프링부트에서도 이 방식을 기본값으로 사용하고 있다
