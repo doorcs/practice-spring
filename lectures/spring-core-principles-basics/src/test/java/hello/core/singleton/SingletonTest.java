8b95d88665896cb16c0a87186a1e67af124e5dee
doorcs <itsdoorcs@gmail.com>
2026-03-24 14:00:49 +0900

    싱글톤 컨테이너
    스프링 컨테이너(싱글톤 컨테이너)를 활용하면!
    싱글톤 패턴을 직접 적용하지 않아도 객체 인스턴스들을 싱글톤으로 사용할 수 있다

--- lectures/spring-core-principles-basics/src/test/java/hello/core/singleton/SingletonTest.java
+++ lectures/spring-core-principles-basics/src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class SingletonTest {
 
@@ -45,5 +47,21 @@ class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
         // `same`은 `==`를 사용해서 비교한다 (참조값이 같은지, 즉 같은 객체인지 비교)
         // `equal`은 `equals()` 메서드를 사용해서 비교한다 (내용물이 같은지 비교한다)
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 같다! (같은 객체를 공유하기 때문)
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
