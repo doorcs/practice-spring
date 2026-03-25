f50a7de657261ee8f00ba3aea9864913d07fd042
doorcs <itsdoorcs@gmail.com>
2026-03-25 11:34:43 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점
    싱글톤 빈에서 프로토타입 빈을 주입받아 사용하게 되면
    싱글톤 빈이 처음 등록될 때 딱 한번만 프로토타입 빈을 요청하게 된다

---
+++ lectures/spring-core-principles-basics/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,73 @@
+package hello.core.scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+class SingletonWithPrototypeTest1 {
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(2);
+
+        // 이 테스트가 성공하는것은 의도한 결과가 아니다!
+
+        // 프로토타입 빈을 쓴다는건 요청마다 새로운 빈을 사용하겠다는 뜻인데,
+        // 싱글톤 빈인 `ClientBean`이 스프링 컨테이너에 등록될 때 프로토타입 빈인 `PrototypeBean`을 요청해 주입받고
+        // 이걸 계속해서 재사용하게 된다
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+
+        private final PrototypeBean prototypeBean;
+
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
