1f703782c9acf50f9272abc23a4e522a7f0af540
doorcs <itsdoorcs@gmail.com>
2026-03-31 15:28:33 +0900

    HttpServletResponse - 기본 사용법
    직접 `setStatus()`, `setHeader()`를 호출해서 모두 처리할 수 있지만,
    `HttpServletResponse` 객체가 제공하는 편의 메서드를 활용하는게 편리하다!!

---
+++ lectures/spring-mvc-part-1/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,62 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // [status-line]
+        response.setStatus(HttpServletResponse.SC_OK); // 직접 `200`을 적는것보다 상수를 쓰는게 더 낫다! (매직 넘버 사용을 피해라)
+
+        // [response-headers]
+        response.setHeader("Content-Type", "text/plain");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("my-header", "hello");
+
+        // [Header 편의 메서드]
+        // content(response);
+        // cookie(response);
+        // redirect(response);
+
+        PrintWriter writer = response.getWriter();
+        writer.println("ok");
+    }
+
+    private void content(HttpServletResponse response) {
+        // Content-Type: text/plain;charset=utf-8
+        // Content-Length: 2
+
+        // response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        // response.setContentLength(2); // 생략시 바디의 길이를 자동으로 계산해서 생성해준다
+    }
+
+    private void cookie(HttpServletResponse response) {
+        // Set-Cookie: myCookie=good; Max-Age=600;
+
+        // response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600); // 600초
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException {
+        // Status Code 302
+        // Location: /basic/hello-form.html
+
+        // response.setStatus(HttpServletResponse.SC_FOUND); // 302
+        // response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html"); // HTTP `상태코드가 302`이면서 `Location` 헤더가 있으니 리디렉션됨
+    }
+}
