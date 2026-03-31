d123c505407986ed0f11b8f7ea28aa447fc962ae
doorcs <itsdoorcs@gmail.com>
2026-03-31 16:05:20 +0900

    회원 관리 웹 애플리케이션 요구사항
    도메인 + 싱글톤 리포지토리 + 기본적인 테스트코드 작성

---
+++ lectures/spring-mvc-part-1/src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,38 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>(); // 실무에서는 ConcurrentHashMap 사용 고려
+    private static long sequence = 0L;
+
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    private MemberRepository() {}
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear(); // 테스트에서 사용하기 위한 메서드
+    }
+}
