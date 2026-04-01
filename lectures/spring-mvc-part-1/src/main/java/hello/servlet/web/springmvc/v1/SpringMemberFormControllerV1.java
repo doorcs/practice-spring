d2443ea1b03593a9a38a2eb037885da6e0ad01d6
doorcs <itsdoorcs@gmail.com>
2026-04-01 11:59:33 +0900

    스프링 MVC - 시작하기
    스프링 부트 3.0(스프링 6.0) 부터는
    클래스 레벨에 `@RequestMapping`이 있어도 스프링 컨트롤러로 인식하지 않는다!!
    
    즉, RequestMappingHandlerMapping 에서
    `@RequestMapping`은 더 이상 인식하지 않고,
    `@Controller`만 인식한다

---
+++ lectures/spring-mvc-part-1/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,14 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller // `@Controller`는 `@Component` 어노테이션을 포함하고 있다!
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
