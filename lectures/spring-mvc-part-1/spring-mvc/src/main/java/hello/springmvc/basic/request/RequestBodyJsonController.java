30164ef24bc171468a13c052fb0773677ca81f07
doorcs <itsdoorcs@gmail.com>
2026-04-01 21:41:30 +0900

    HTTP 요청 메시지 - JSON
    `@RequestBody` 요청
    - 클라이언트가 HTTP 요청 바디에 JSON을 담아서 보냄
    - HTTP 메시지 컨버터가 동작함
    - 객체로 변환
    
    `@ResponseBody` 응답
    - 객체를 return
    - HTTP 메시지 컨버터가 동작함
    - JSON으로 변환해서 HTTP 응답 바디에 담고, 클라이언트에게 응답

---
+++ lectures/spring-mvc-part-1/spring-mvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -0,0 +1,76 @@
+package hello.springmvc.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.springmvc.basic.HelloData;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class RequestBodyJsonController {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostMapping("/request-body-json-v1")
+    public void requestBodyJsonV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody = {}", messageBody);
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+
+        response.getWriter().write("ok");
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v2")
+    public String requestBodyJsonV2(@RequestBody String messageBody) throws IOException {
+
+        log.info("messageBody = {}", messageBody);
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v3")
+    public String requestBodyJsonV3(@RequestBody HelloData helloData) {
+
+        // HTTP 메시지 컨버터 (MappingJackson2HttpMessageConverter)가 v2에서 했던 작업을 대신 처리해준다!
+        // HTTP 요청 바디의 내용을 읽어야 하기 때문에, `@RequestBody` 어노테이션을 생략하면 안 된다! (생략하면 `@ModelAttribute`로 동작)
+        log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v4")
+    public String requestBodyJsonV4(HttpEntity<HelloData> httpEntity) {
+
+        // `HttpEntity`를 직접 사용할수도 있다
+        // 이때 메시지 컨버터는 메서드의 파라미터를 주입해주는 시점에 동작한다! `.getBody()`는 이미 변환이 완료되어 있는 객체를 꺼내오는 것
+        HelloData data = httpEntity.getBody();
+        log.info("username = {}, age = {}", data.getUsername(), data.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v5")
+    public HelloData requestBodyJsonV5(@RequestBody HelloData data) {
+        log.info("username = {}, age = {}", data.getUsername(), data.getAge());
+
+        // HTTP 메시지 컨버터는 입력 뿐만 아니라 반환(return)에도 적용된다!
+        return data;
+    }
+}
