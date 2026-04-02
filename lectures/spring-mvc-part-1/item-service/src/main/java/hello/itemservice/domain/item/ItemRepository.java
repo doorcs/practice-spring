cc5c95eb94a23467c024ef5312ec4ffcfab51686
doorcs <itsdoorcs@gmail.com>
2026-04-02 11:07:52 +0900

    상품 도메인 개발

---
+++ lectures/spring-mvc-part-1/item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,41 @@
+package hello.itemservice.domain.item;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Item save(Item item) {
+        item.setId(sequence++);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        // 여기서는 `updateParam.getId()`, `updateParam.setId()` 가 사용되지 않음 + 실수가 생길 여지가 있다!!
+        // 그래서 정석대로 하려면 `ItemParamDto 같은 별도의 객체`를 만들어서 `변경에 필요한 파라미터 3개만 담도록` 처리하는게 맞다
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
