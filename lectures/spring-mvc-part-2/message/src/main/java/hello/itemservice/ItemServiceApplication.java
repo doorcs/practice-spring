dc9524a5539a904ffb68e629ef79b9554ad34e73
doorcs <itsdoorcs@gmail.com>
2026-04-09 00:07:41 +0900

    스프링 메시지 소스 설정

--- lectures/spring-mvc-part-2/message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ lectures/spring-mvc-part-2/message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,9 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.MessageSource;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -9,5 +12,14 @@ public class ItemServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
+
+// 스프링 부트를 쓰면 이런 빈을 자동으로 만들어준다! 직접 등록 안해도 됨
+//	@Bean
+//	public MessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasenames("messages", "errors");
+//		messageSource.setDefaultEncoding("UTF-8");
+//		return messageSource;
+//	}
 
 }
