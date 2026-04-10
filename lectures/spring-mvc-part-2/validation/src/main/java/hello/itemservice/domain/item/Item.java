5ecfe5f92a898e417e54d0c89b21b4089199ef72
doorcs <itsdoorcs@gmail.com>
2026-04-10 19:59:37 +0900

    Bean Validation - 시작
    `@NotNull`, `@Range`, `@Max`, ...

--- lectures/spring-mvc-part-2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ lectures/spring-mvc-part-2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,23 @@
 package hello.itemservice.domain.item;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotNull
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1_000_000) // `@Range`는 hibernate 구현체에서만 동작함!
     private Integer price;
+
+    @Max(9999)
     private Integer quantity;
 
     public Item() {}
