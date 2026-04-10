cca1d58efdd558c4777fb8a6f6de77af432408b8
doorcs <itsdoorcs@gmail.com>
2026-04-10 23:52:45 +0900

    Bean Validation - groups
    실무에서는 groups 기능을 잘 사용하지 않는다:
    1. 너무 복잡하다. 복잡도가 올라간다
    2. 등록용 폼 객체와 수정용 폼 객체를 분리해서 사용하면 더 간단하게 해결할 수 있는 문제다
    
    꼭 폼 데이터가 아니더라도, 상황에 따라 커맨드 객체를 다르게 사용하는 경우가 많음

--- lectures/spring-mvc-part-2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ lectures/spring-mvc-part-2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,5 +1,7 @@
 package hello.itemservice.domain.item;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
@@ -8,18 +10,22 @@ import org.hibernate.validator.constraints.Range;
 // @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
 public class Item {
 
-    @NotNull
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotNull
+    // 문자열에는 `@NotNull` 보다 `@NotBlank` 가 맞지 않나..?
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1_000_000) // `@Range`는 hibernate 구현체에서만 동작함!
+    @Range(
+            min = 1000,
+            max = 1_000_000,
+            groups = {SaveCheck.class, UpdateCheck.class}) // `@Range`는 hibernate 구현체에서만 동작함!
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    // @Max(9999)
-    @NotNull
+    @Max(value = 9999, groups = SaveCheck.class)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
     private Integer quantity;
 
     public Item() {}
--- lectures/spring-mvc-part-2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ lectures/spring-mvc-part-2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -2,6 +2,8 @@ package hello.itemservice.web.validation;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -43,7 +45,7 @@ public class ValidationItemControllerV3 {
 
     @PostMapping("/add")
     public String addItem(
-            @Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+            @Validated(SaveCheck.class) @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         // 특정 필드 예외가 아닌 전체 예외
         if (item.getPrice() != null && item.getQuantity() != null) {
@@ -76,7 +78,7 @@ public class ValidationItemControllerV3 {
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+    public String edit(@PathVariable Long itemId, @Validated(UpdateCheck.class) @ModelAttribute Item item, BindingResult bindingResult) {
 
         // 특정 필드 예외가 아닌 전체 예외
         if (item.getPrice() != null && item.getQuantity() != null) {
