09d41954e1750651ed0113b152c4679bf010d2a0
doorcs <itsdoorcs@gmail.com>
2026-04-11 17:04:31 +0900

    로그인 처리하기 - 서블릿 HTTP 세션1
    `HttpServletRequest.getSession()`
    
    기본값은 `.getSession(true)` 즉, 세션이 없다면 새로 생성
    
    그래서 기존 세션을 찾아 사용하는 시점엔 `.getSession(false)`를 통해 의미 없는 새 세션 생성을 막아줘야한다!!!

--- lectures/spring-mvc-part-2/login/src/main/java/hello/login/web/HomeController.java
+++ lectures/spring-mvc-part-2/login/src/main/java/hello/login/web/HomeController.java
@@ -6,6 +6,7 @@ import hello.login.web.session.SessionManager;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -43,7 +44,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    // @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
 
         // 세션 관리자에 저장된 회원 정보 조회
@@ -53,6 +54,26 @@ public class HomeController {
         }
 
         model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 회원 데이터가 없으면 홈으로
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인 홈으로
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 
--- lectures/spring-mvc-part-2/login/src/main/java/hello/login/web/login/LoginController.java
+++ lectures/spring-mvc-part-2/login/src/main/java/hello/login/web/login/LoginController.java
@@ -2,10 +2,12 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
 import hello.login.web.session.SessionManager;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -61,7 +63,7 @@ public class LoginController {
         response.addCookie(cookie);
     }
 
-    @PostMapping("/login")
+    // @PostMapping("/login")
     public String loginV2(
             @Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
 
@@ -81,9 +83,44 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/logout")
+    // @PostMapping("/logout")
     public String logoutV2(HttpServletRequest request) {
         sessionManager.expire(request);
+        return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String loginV3(
+            @Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        log.info("login? {}", loginMember);
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 잘못되었습니다.");
+            return "login/loginForm";
+        }
+
+        // 세션이 있으면 세션 반환, 없으면 신규 세션 생성
+        HttpSession session = request.getSession();
+
+        // 세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logoutV3(HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+        if (session != null) {
+            session.invalidate();
+        }
         return "redirect:/";
     }
 }
