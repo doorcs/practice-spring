7e42d27d77da7f46a438d4a3c2f24b3a8e43d7cc
doorcs <itsdoorcs@gmail.com>
2026-04-13 20:02:58 +0900

    서블릿 필터 - 요청 로그
    0. `jakarta.servlet.Filter` 인터페이스를 implement하는 클래스 만들기
    1. `ServletRequest` -> `HttpServletRequest` 다운캐스팅 필요!
    2. 구현한 필터를 스프링 빈으로 등록해주기
    (`FilterRegistrationBean` 활용)

---
+++ lectures/spring-mvc-part-2/login/src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,30 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    // @Bean
+    // public FilterRegistrationBean logFilter() {
+    //     FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean<>();
+    //     filterRegistrationBean.setFilter(new LogFilter());
+    //     filterRegistrationBean.setOrder(1);
+    //     filterRegistrationBean.addUrlPatterns("/*");
+
+    //     return filterRegistrationBean;
+    // }
+
+    @Bean // 구현한 필터를 빈으로 등록
+    public FilterRegistrationBean<LogFilter> logFilter() { // 템플릿 파라미터로 필터 타입 넣어주기!
+        FilterRegistrationBean<LogFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1); // 이 필터가 필터 체인에서 가장 먼저 실행되도록 지정
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
