01d1e27da625e7e5af666811542428dbac5419dc
doorcs <itsdoorcs@gmail.com>
2026-04-13 15:11:46 +0900

    로그인 처리하기 - 서블릿 HTTP 세션2
    `@SessionAttribute` 스프링에서 제공해주는 세션 활용

--- lectures/spring-mvc-part-2/login/src/main/java/hello/login/web/HomeController.java
+++ lectures/spring-mvc-part-2/login/src/main/java/hello/login/web/HomeController.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
@@ -57,7 +57,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    // @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         HttpSession session = request.getSession(false);
@@ -77,7 +77,21 @@ public class HomeController {
         return "loginHome";
     }
 
-    @PostMapping("/logout")
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        // 세션에 회원 데이터가 없으면 홈으로
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인 홈으로
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    // @PostMapping("/logout")
     public String logout(HttpServletResponse response) {
         expireCookie(response, "memberId");
         return "redirect:/";
