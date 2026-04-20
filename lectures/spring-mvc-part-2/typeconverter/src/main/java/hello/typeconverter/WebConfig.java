525d4ca965c495f1ed87769565060e4759decfa9
doorcs <itsdoorcs@gmail.com>
2026-04-20 13:27:17 +0900

    포매터 적용하기
    컨버터와 포매터의 적용 대상이 겹치면?
    -> 컨버터가 우선이다

--- lectures/spring-mvc-part-2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ lectures/spring-mvc-part-2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -1,9 +1,8 @@
 package hello.typeconverter;
 
-import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
-import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +12,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        // 컨버터와 포매터가 겹칠 경우 컨버터가 우선이기 때문에, 포매터를 적용해보기 위해 컨버터는 주석 처리
+        // registry.addConverter(new StringToIntegerConverter());
+        // registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
