9311536039b3fa2576850fa4a6d11852e858619f
doorcs <itsdoorcs@gmail.com>
2026-04-24 01:44:25 +0900

    JDBC 개발 - 등록
    Connection conn = getConnection()
    PreparedStatement stmt = conn.prepareStatement()
    pstmt.setXxx()
    pstmt.executeUpdate()
    
    이후 `사용한 리소스 close()`시 예외처리 주의!!

---
+++ lectures/spring-db-part-1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,65 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import java.sql.*;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MemberRepositoryV0 {
+
+    public Member save(Member member) throws SQLException {
+        String sql = "INSERT INTO member(member_id, money) VALUES(?, ?)";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate(); // 준비한 쿼리를 실제로 DB에 전달해서 실행
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            // 이런애들도 실행하다가 중간에 예외가 터질 수 있음 -> try-catch로 다 잡아줘야함 ㅋㅋㅋ
+            // pstmt.close();
+            // conn.close();
+            close(conn, pstmt, null);
+        }
+    }
+
+    private void close(Connection conn, Statement stmt, ResultSet rs) {
+
+        if (rs != null) { // 이건 나중에 쓸 건데 미리 만들어두는것
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+}
