193c141ee22e05a6045268caff9be5a235109bbb
doorcs <itsdoorcs@gmail.com>
2026-04-23 23:50:26 +0900

    프로젝트 생성
    2026.04.23
    Spring Boot 3.5.13
    Java 21

---
+++ lectures/spring-db-part-1/jdbc/src/test/java/hello/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,11 @@
+package hello.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
