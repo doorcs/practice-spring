dbeb698d19258bf7cad12f475ec4b3866dd521d8
doorcs <itsdoorcs@gmail.com>
2026-04-25 23:38:47 +0900

    DataSource 예제2 - 커넥션 풀
    커넥션 풀을 채우는 일은 오래 걸림
    -> 전체적인 애플리케이션 실행을 늦추지 않기 위해, 커넥션 풀을 채우는 작업은 별도의 쓰레드에서 수행!
    
    풀에서 커넥션 획득을 시도할 때, 실패하면 일단 조금 기다려본다
    -> Timeout 시간을 넘기면 예외 발생

--- lectures/spring-db-part-1/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ lectures/spring-db-part-1/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -26,10 +27,39 @@ class ConnectionTest {
         useDataSource(dataSource); // 사용 - 사용하는 곳에서는 설정과 관련된 정보를 몰라도 된다!!!
     }
 
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000); // 커넥션 풀에서 커넥션 생성 시간 대기
+    }
+
     private void useDataSource(DataSource dataSource) throws SQLException {
-        Connection con1 = dataSource.getConnection(); // DataSource 객체를 생성할 때 설정 정보(URL, USERNAME, PW)를 넣어줬기 때문에
-        Connection con2 = dataSource.getConnection(); // 커넥션 획득시에는 매번 설정 정보들을 넘겨주지 않아도 된다!
-        log.info("connection={}, class={}", con1, con1.getClass());
-        log.info("connection={}, class={}", con2, con2.getClass());
+        Connection conn1 = dataSource.getConnection(); // DataSource 객체를 생성할 때 설정 정보(URL, USERNAME, PW)를 넣어줬기 때문에
+        Connection conn2 = dataSource.getConnection(); // 커넥션 획득시에는 매번 설정 정보들을 넘겨주지 않아도 된다!
+        // Connection conn3 = dataSource.getConnection();
+        // Connection conn4 = dataSource.getConnection();
+        // Connection conn5 = dataSource.getConnection();
+        // Connection conn6 = dataSource.getConnection();
+        // Connection conn7 = dataSource.getConnection();
+        // Connection conn8 = dataSource.getConnection();
+        // Connection conn9 = dataSource.getConnection();
+        // Connection conn10 = dataSource.getConnection();
+        // Connection conn11 = dataSource.getConnection(); // 요청시 커넥션 풀에 남은 커넥션이 없음 -> 일단 기다림 -> Timeout(30초) -> 예외 발생
+        log.info("connection = {}, class = {}", conn1, conn1.getClass());
+        log.info("connection = {}, class = {}", conn2, conn2.getClass());
     }
 }
+
+// 23:32:06.989 [MyPool:connection-adder] DEBUG com.zaxxer.hikari.pool.HikariPool --MyPool - Connection not added, stats (total=10/10, idle=0/10, active=10, waiting=1)
+// 23:32:36.710 [MyPool:housekeeper] DEBUG com.zaxxer.hikari.pool.HikariPool --MyPool - Fill pool skipped, pool has sufficient level or currently being filled.
+// 23:32:36.961 [Test worker] DEBUG com.zaxxer.hikari.pool.HikariPool --MyPool - Timeout failure stats (total=10/10, idle=0/10, active=10, waiting=0)
+//
+// MyPool - Connection is not available, request timed out after 29999ms (total=10, active=10, idle=0, waiting=0)
+// java.sql.SQLTransientConnectionException: MyPool - Connection is not available, request timed out after 29999ms (total=10, active=10, idle=0, waiting=0)
