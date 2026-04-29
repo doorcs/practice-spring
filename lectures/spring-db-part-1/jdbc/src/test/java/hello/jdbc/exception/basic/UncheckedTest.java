4a40c6f45e7c851c26348b716b734371e22f212e
doorcs <itsdoorcs@gmail.com>
2026-04-29 11:43:04 +0900

    언체크 예외 기본 이해
    언체크 예외는 잡아서 처리하지 않는 경우에도 throws 구문을 생략할 수 있다!

---
+++ lectures/spring-db-part-1/jdbc/src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
@@ -0,0 +1,53 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+class UncheckedTest {
+
+    @Test
+    void unchecked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void unchecked_throw() {
+        Service service = new Service();
+        assertThatThrownBy(() -> service.callThrow()).isInstanceOf(MyUncheckedException.class);
+    }
+
+    static class MyUncheckedException extends RuntimeException {
+        public MyUncheckedException(String message) {
+            super(message);
+        }
+    }
+
+    static class Service {
+
+        Repository repository = new Repository();
+
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyUncheckedException e) {
+                log.info("예외 처리, message = {}", e.getMessage(), e);
+            }
+        }
+
+        // public void callThrow() throws MyUncheckedException {
+        public void callThrow() throws MyUncheckedException {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+
+        // public void call() throws MyUncheckedException {
+        public void call() {
+            throw new MyUncheckedException("ex");
+        }
+    }
+}
